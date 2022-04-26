3c126fe45c1d4f60391f44bbad6787dbeca29a60
Chetan <cipher@pop-os.localdomain>
2022-04-26 17:51:08 +0530

    ERC115 NFT contract

--- contracts/ERC115NFT.sol
+++ contracts/ERC115NFT.sol
@@ -4,11 +4,15 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract BitcoinERC1155 is ERC1155 {
+contract ERC115NFT is ERC1155 {
     using Counters for Counters.Counter;
     Counters.Counter private _nftID;
 
-    constructor() public ERC1155("") {}
+    constructor()
+        ERC1155(
+            "https://gateway.pinata.cloud/ipfs/QmZbF3TXTXMSqqhkzwmZ33vYzxThQxMMdcPfjBUFDkihnb"
+        )
+    {}
 
     function mint(bytes memory data) public returns (uint256 NFTId) {
         _nftID.increment();

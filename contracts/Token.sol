dd6dc457c725caaad79b8ef8c906aff0a6ef9ef9
Chetan <cipher@pop-os.localdomain>
2022-05-06 13:50:07 +0530

    removed unnecessary variables

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -6,8 +6,6 @@ import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 contract Token is ERC1155 {
     uint256 public constant COIN = 1;
 
-    mapping(uint256 => string) metadataRegistry;
-
     constructor() ERC1155("") {
         _mint(msg.sender, COIN, 10000 * 10**18, "");
     }
